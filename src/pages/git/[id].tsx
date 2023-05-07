45de3886b40bd10cf8cbda0851ea10a7bdeb9c0b
noah.hsu <xuhaojuntw@gmail.com>
2023-05-07 23:43:13 +0800

    feat: add side bar

--- src/pages/git/[id].tsx
+++ src/pages/git/[id].tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import type { WheelEvent } from "react";
 
 import ReactFlow, {
@@ -15,7 +15,7 @@ import ReactFlow, {
   useKeyPress,
 } from "reactflow";
 // import "reactflow/dist/style.css";
-import { Row, Col, Layout } from "antd";
+import { Row, Col, Layout, Breadcrumb, Menu, MenuProps } from "antd";
 
 import { Inter } from "next/font/google";
 
@@ -27,11 +27,19 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { Scrollbars } from "react-custom-scrollbars-2";
 import { useRootStore } from "@/stores/root.store";
-import { take } from "rambda";
+import { clamp, isEmpty, sort, tail, take } from "rambda";
 import MinimapScrollbar from "../../components/Scrollbars/MinimapScrollbar";
 import GitTable from "@/components/GitTable/GitTable";
 import ResizableColumns from "@/components/ResizableColumns";
 import styled from "styled-components";
+import {
+  FolderOutlined,
+  BranchesOutlined,
+  CloudOutlined,
+  LaptopOutlined,
+} from "@ant-design/icons";
+
+type MenuItem = Required<MenuProps>["items"][number];
 
 const { Header, Content, Footer, Sider } = Layout;
 // import { Header, Content, Footer } from "antd/es/layout/layout";
@@ -179,15 +187,73 @@ export default function GitPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const items: MenuItem[] = useMemo(
+    () => [
+      {
+        key: "local",
+        label: "LOCAL",
+        icon: <LaptopOutlined />,
+        children: gitResp.branches
+          ? getLocalBranchesMenuItems(gitResp.branches)
+          : [],
+      },
+      {
+        key: "remote",
+        label: "REMOTE",
+        icon: <CloudOutlined />,
+        children: gitResp?.branches
+          ? getRemoteBranchesMenuItems(gitResp.branches)
+          : [],
+      },
+      {
+        key: "issues",
+        label: "ISSUES",
+      },
+      {
+        key: "tags",
+        label: "TAGS",
+      },
+    ],
+    [gitResp?.branches]
+  );
+
   return (
-    <Layout style={{ display: "flex", flexGrow: 1 }}>
-      <Header style={{ height: 84 }}>
-        <div className="logo" />
-      </Header>
-      <Layout>
-        <Sider width={300}></Sider>
-        <Content>
-          {/* <ol>
+    <RootStyledDiv>
+      <Layout style={{ display: "flex", flexGrow: 1 }}>
+        <Header style={{ height: 84, background: "#ffffff" }}>
+          {/* <Breadcrumb
+          separator=">"
+          items={[
+            {
+              title: "Home",
+            },
+            {
+              title: "Application Center",
+              href: "",
+            },
+            {
+              title: "Application List",
+              href: "",
+            },
+            {
+              title: "An Application",
+            },
+          ]}
+        /> */}
+        </Header>
+        <Layout>
+          <Sider width={300} className="xu-left-sider">
+            <Menu
+              style={{ overflow: "auto" }}
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={[]}
+              mode="inline"
+              theme="dark"
+              items={items}
+            />
+          </Sider>
+          <Content className="xu-content">
+            {/* <ol>
         {commits.map((x) => (
           <li key={x.oid}>
             <a>{take(12, x.oid)}</a>&nbsp;{x.author.name}&nbsp;{x.author.email}
@@ -208,11 +274,9 @@ export default function GitPage() {
           });
         }}
       </Gitgraph> */}
-          <StyledContentContainer>
             <GitTable dataSource={gitResp?.commits || []}></GitTable>
-          </StyledContentContainer>
-          {/* <ResizableColumns/> */}
-          {/* <MinimapScrollbar style={{ width: "100vw", height: "100vh" }}>
+            {/* <ResizableColumns/> */}
+            {/* <MinimapScrollbar style={{ width: "100vw", height: "100vh" }}>
         <ol>
           {take(150, gitResp?.commits || []).map((x: any) => (
             <li key={x.oid}>
@@ -221,19 +285,106 @@ export default function GitPage() {
           ))}
         </ol>
       </MinimapScrollbar> */}
-          {/* <div style={{ width: "100vw", height: "90vh" }}>
+            {/* <div style={{ width: "100vw", height: "90vh" }}>
         <ReactFlowProvider>
           <Flow commits={data?.commits || []} />
         </ReactFlowProvider>
       </div> */}
-        </Content>
-        <Sider width={400}></Sider>
+          </Content>
+          <Sider width={400}></Sider>
+        </Layout>
+        <Footer style={{ height: "28px", padding: 0 }}>
+          status bar like vscode
+        </Footer>
       </Layout>
-      <Footer style={{ height: "28px", padding: 0 }}>status bar like vscode</Footer>
-    </Layout>
+    </RootStyledDiv>
   );
 }
 
-const StyledContentContainer = styled.div`
-  height: calc(100vh - 46px - 84px - 28px);
+const RootStyledDiv = styled.div`
+  --content-height: calc(100vh - 46px - 84px - 28px);
+  .xu-content {
+    height: var(--content-height);
+  }
+  .xu-left-sider {
+    height: var(--content-height);
+    max-height: var(--content-height);
+    overflow: auto;
+  }
 `;
+
+interface GitBranch {
+  name: string;
+  oid: string;
+  branch_type: "local" | "remote";
+}
+
+function gitBranchesToMenuItems(branches: GitBranch[]): MenuItem[] {
+  interface Node {
+    name: string;
+    branch: GitBranch;
+    children: Node[];
+  }
+
+  const root: Pick<Node, "children"> = { children: [] };
+  for (const branch of branches) {
+    let currentNode = root;
+    for (const name of branch.name.split("/")) {
+      if (!name) continue;
+      let childNode: Node | undefined = currentNode.children.find(
+        (child) => child.name === name
+      );
+      if (!childNode) {
+        childNode = {
+          name,
+          branch,
+          children: [],
+        };
+        currentNode.children.push(childNode);
+      }
+      currentNode = childNode;
+    }
+  }
+
+  const sorter = sort<Node>((a, b) => {
+    if (clamp(0, 1, a.children.length) > clamp(0, 1, b.children.length))
+      return -1;
+    if (clamp(0, 1, a.children.length) < clamp(0, 1, b.children.length))
+      return 1;
+    const nameCompare = a.name
+      .toLowerCase()
+      .localeCompare(b.name.toLowerCase());
+    if (nameCompare !== 0) return nameCompare;
+    return 0;
+  });
+  function nodesToMenuItems(xs: Node[]): MenuItem[] {
+    return sorter(xs).map((x) => {
+      const hasChildren = !isEmpty(x.children);
+
+      const children = hasChildren ? nodesToMenuItems(x.children) : undefined;
+      const icon = hasChildren ? <FolderOutlined /> : <BranchesOutlined />;
+      return {
+        key: x.branch.name,
+        label: x.name,
+        icon,
+        children,
+      };
+    });
+  }
+
+  return nodesToMenuItems(root.children);
+}
+
+function getRemoteBranchesMenuItems(brances: GitBranch[]): MenuItem[] {
+  return gitBranchesToMenuItems(
+    brances
+      .filter((x) => x.branch_type === "remote")
+      .map((x) => ({ ...x, name: tail(x.name.split("/")).join("/") }))
+  );
+}
+
+function getLocalBranchesMenuItems(brances: GitBranch[]): MenuItem[] {
+  return gitBranchesToMenuItems(
+    brances.filter((x) => x.branch_type === "local")
+  );
+}
